2b2e1bf89be7859f90ec6fef8a57777f37d603d9
Frank Bieberly <frank.bieberly@gmail.com>
2019-03-07 11:03:09 -0600

    initial list of orbcomm satellites and frequencies (got it from TylerK's code

---
+++ sat_db.py
@@ -0,0 +1,46 @@
+# List of the orbcomm satellites that I currently know are transmitting
+
+
+
+active_orbcomm_satellites = {
+							'orbcomm fm116':{
+											'norad_id':41189,
+											'frequencies':[137662500.0, 137800000.0]
+											},
+							'orbcomm fm115':{
+											'norad_id':41186,
+											'frequencies':[137460000.0, 137717500.0]
+											},
+							'orbcomm fm108':{
+											'norad_id':41187,
+											'frequencies':[137460000.0, 137717500.0]
+											},
+							'orbcomm fm113':{
+											'norad_id':41185,
+											'frequencies':[137662500.0, 137800000.0]
+											},
+							'orbcomm fm112':{
+											'norad_id':41184,
+											'frequencies':[137662500.0, 137800000.0]
+											},
+							'orbcomm fm110':{
+											'norad_id':41182,
+											'frequencies':[137287500.0, 137737500.0]
+											},
+							'orbcomm fm114':{
+											'norad_id':41179,
+											'frequencies':[137287500.0, 137737500.0]
+											},
+							'orbcomm fm103':{
+											'norad_id':40091,
+											'frequencies':[137250000.0, 137312500.0]
+											},
+							'orbcomm fm107':{
+											'norad_id':40087,
+											'frequencies':[137250000.0, 137312500.0]
+											},
+							'orbcomm fm109':{
+											'norad_id':40086,
+											'frequencies':[137250000.0, 137312500.0]
+											}
+							}

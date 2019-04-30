6479d152023c4217df7defa0303d8797e853b326
Frank Bieberly <frank.bieberly@gmail.com>
2019-04-30 16:20:29 -0700

    add bit decoder

--- file_decoder.py
+++ file_decoder.py
@@ -1,4 +1,15 @@
-
+##############################################################################
+#
+# Author: Frank Bieberly
+# Date: 30 April 2019
+# Name: file_decoder.py
+# Description: 
+# This script takes in .mat files (produced by record_spectrum_orbcomm.py) and
+# produces multiple plots of the signals spectrum, constellation, timing 
+# recovery, eye diagram, and IQ samples. Additionally, decoded bits are printed
+# and saved to a file.
+#
+##############################################################################
 
 import time
 import glob
@@ -30,7 +41,7 @@ obs.elevation = alt # Technically is the altitude of observer
 
 # Where the data files are located
 data_dir = r'./data/'
-sample_file = sorted(glob.glob(data_dir + "*.mat"))[5]
+sample_file = sorted(glob.glob(data_dir + "*.mat"))[0]
 
 # Load the .mat file and print some of the metadata
 data = loadmat(sample_file)
@@ -79,7 +90,7 @@ filtered_samples = butter_lowpass_filter(mixed_down_samples, filter_freq, sample
 
 # Decimated signal
 baud_rate = 4800.0
-samples_per_symbol = 8
+samples_per_symbol = 2  # We should only need 2 samples per symbol
 decimation = int(sample_rate/(samples_per_symbol*baud_rate))
 decimated_samples = filtered_samples[::decimation]
 
@@ -89,10 +100,11 @@ decimated_samples = filtered_samples[::decimation]
 rbw = 1
 nperseg = int(sample_rate/decimation/rbw)
 signal_to_4th_power = np.power(decimated_samples, 4)
-f, pxx = scisig.welch(signal_to_4th_power, fs=sample_rate/decimation, nperseg=nperseg, scaling='density')
+f, pxx = scisig.welch(signal_to_4th_power, fs=sample_rate/decimation, \
+                      return_onesided=False, nperseg=nperseg, scaling='density')
 f = (np.roll(f, len(f)/2))
 pxx = np.roll(pxx, len(pxx)/2)
-search_window = int(1000 / ((sample_rate/decimation)/nperseg))  # search +/- 1 kHz around fc
+search_window = int(1000. / ((sample_rate/decimation)/nperseg))  # search +/- 1 kHz around fc
 frequency_peak = np.argmax(pxx[len(pxx)/2 - search_window:len(pxx)/2 + search_window])
 freq_offset = -(frequency_peak - search_window)*(sample_rate/decimation/nperseg) / 4
 baseband_samples = complex_mix(decimated_samples, freq_offset, sample_rate/decimation)
@@ -102,19 +114,20 @@ print "Remaining frequency offset: {}".format(freq_offset)
 alpha = 0.4
 baudrate = 1.
 num_of_symbols_half_filter = 8.
-rrc_num_taps = samples_per_symbol * num_of_symbols_half_filter * 2.
+rrc_num_taps = samples_per_symbol * num_of_symbols_half_filter * 2. + 1.
 t_idx, rrc_taps = rrcosfilter(rrc_num_taps, alpha, baudrate, samples_per_symbol)
 matched_filtered_samples = scisig.lfilter(rrc_taps, [1.0], baseband_samples)
 
 # Manually find a close timing offset
-sample_delay = 2
+# For only 2 samples per symbol, 0 works all the time.
+sample_delay = 0
 
 tau     = 0.                        # initial timing offset estimate
 dtau    = 0.                        # initial timing _rate_ offset estimate
 buf     = np.zeros(5, dtype=np.complex64) # filter buffer
 th      = np.array([-2., -1., 0., 1., 2.]) # time vector for interpolating filter
 w       = np.sqrt(np.hamming(5).T)  # window function for interpolating filter
-alpha   = 0.005                    # loop filter bandwidth (timing _rate_ adjustment factor)
+alpha   = 0.0005                    # loop filter bandwidth (timing _rate_ adjustment factor)
 beta    = 2*np.sqrt(alpha)          # (timing _phase_ adjustment factor)
 counter = sample_delay + 1          # interpolating filter adds delay
 time_recovery_samples       = np.zeros(len(matched_filtered_samples), dtype=np.complex64)
@@ -161,19 +174,22 @@ for i in range(1, len(matched_filtered_samples)):
     dtau_vect[i] = dtau
     tau_vect[i] = tau
 
+# Plot timing offset
 plt.figure()
 plt.subplot(211)
-plt.title("Tau")
+plt.title("Timing offset (tau)")
 plt.plot(tau_vect)
 
 plt.subplot(212)
-plt.title("DTau")
+plt.title("Derivative (Dtau)")
 plt.plot(dtau_vect)
+plt.tight_layout()
 
+# Plot eye-diagram
 plt.figure()
 plt.subplot(311)
-plt.title("Before matched filter")
-offset = 2000
+plt.title("Eye Diagram before matched filter")
+offset = samples_per_symbol * 200
 num_plots = 8
 length = 64
 for xx in xrange(num_plots):
@@ -182,7 +198,7 @@ for xx in xrange(num_plots):
 plt.subplot(312)
 plt.title("After matched filter")
 
-offset = 2000
+offset = samples_per_symbol * 200
 for xx in xrange(num_plots):
     plt.plot(matched_filtered_samples[offset:offset+length].imag)
     offset += length
@@ -190,13 +206,15 @@ plt.grid()
 plt.subplot(313)
 plt.title("After timing recovery")
 
-offset = 2000
+offset = samples_per_symbol * 200
 for xx in xrange(num_plots):
     plt.plot(time_recovery_samples[offset:offset+length].imag)
     offset += length
 plt.grid()
+plt.tight_layout()
 
-
+# After timing recovery, performing a fine-frequency PLL
+# First take the signal to the fourth power, then low pass filter
 filter_freq = 0.1e3
 signal_to_4th_power2 = np.power(time_recovery_samples, 4)
 filtered_sig4th = butter_lowpass_filter(signal_to_4th_power2, filter_freq, sample_rate/decimation, order=5)
@@ -220,6 +238,51 @@ for idx, sample in enumerate(filtered_sig4th):
 # Phase compensate the IQ samples
 phase_comp_samples = time_recovery_samples * np.conj(np.exp(1j*phase_est[:-1]/4.)) * np.conj(np.exp(1j*np.pi/4.))
 
+# Decode to bits
+# normalize phase compensated samples;
+phase_comp_samples /= np.median(np.abs(phase_comp_samples))
+
+# Select peak sample from each symbol
+demod_symbols = phase_comp_samples[::samples_per_symbol]
+
+#Differential demodulation
+# A 1 is a 90 degree phase shift forward from the last symbol
+# A 0 is a -90 degree phase shift forward from the last symbol
+bits = []
+angles = []
+for xx in range(1, len(demod_symbols)):
+    angle = np.angle(demod_symbols[xx], deg=True) - np.angle(demod_symbols[xx-1], deg=True)
+    if angle > 180:
+        angle -= 360
+    if angle < -180:
+        angle += 360
+    angles.append(angle)
+    bit = 0
+    if angle > 0: bit = 1
+    bits.append(bit)
+
+# Plot the angles between each symbol, should be +/- 90 degrees
+plt.figure()
+plt.title('Angle between successive symbols')
+plt.xlabel('symbol number')
+plt.ylabel('Angle (degrees)')
+plt.plot(angles, 'x')
+plt.grid()
+
+# The bits are xor'ed, so xor them again to retreive the original bit stream
+xord_bits = []
+for xx in range(1, len(bits)):
+    xor = 0
+    if np.abs(sum(bits[xx-1:xx])) == 1.0:
+        xor = 1
+    xord_bits.append(xor)
+
+#print out the bits
+print("Bits:")
+print(''.join([str(bit) for bit in xord_bits]))
+
+
+# Plot IQ samples
 plt.figure()
 plt.subplot(211)
 plt.title("After carrier recovery")
@@ -232,13 +295,15 @@ plt.title("After carrier recovery")
 plt.plot(time_recovery_samples[1000:1080].real)
 plt.plot(time_recovery_samples[1000:1080].imag)
 plt.grid()
+plt.tight_layout()
 
 
 # Plot spectrum of recording
 plt.figure()
 plt.subplot(221)
 nperseg = int(sample_rate/100.0)
-f, full_pxx = scisig.welch(samples, fs=sample_rate, nperseg=nperseg, scaling='density')
+f, full_pxx = scisig.welch(samples, fs=sample_rate, nperseg=nperseg, \
+                           return_onesided=False, scaling='density')
 f = (np.roll(f, len(f)/2) + center_freq)/1e6
 full_pxx = np.roll(full_pxx, len(full_pxx)/2)
 full_pxx = 10*np.log10(full_pxx)
@@ -253,14 +318,16 @@ for freq in frequencies:
 # Plot one of the channels as baseband
 plt.subplot(222)
 #plot the decimated signal
-f, pxx = scisig.welch(baseband_samples, fs=sample_rate/decimation, nperseg=nperseg, scaling='density')
+f, pxx = scisig.welch(baseband_samples, fs=sample_rate/decimation, \
+                      return_onesided=False, nperseg=nperseg, scaling='density')
 f = (np.roll(f, len(f)/2))
 pxx = np.roll(pxx, len(pxx)/2)
 pxx = 10*np.log10(pxx)
 plt.plot(f, pxx, label='Decimated')
 
 # plot the low-pass filtered signal
-f, pxx = scisig.welch(filtered_samples, fs=sample_rate, nperseg=nperseg, scaling='density')
+f, pxx = scisig.welch(filtered_samples, fs=sample_rate, nperseg=nperseg, \
+                      return_onesided=False, scaling='density')
 f = (np.roll(f, len(f)/2))
 pxx = np.roll(pxx, len(pxx)/2)
 pxx = 10*np.log10(pxx)
@@ -278,7 +345,8 @@ plt.legend(loc='best')
 # Gives an idea of frequency offset after doppler compensation
 plt.subplot(223)
 nperseg = len(signal_to_4th_power)
-f, pxx = scisig.welch(signal_to_4th_power, fs=sample_rate/decimation, nperseg=nperseg, scaling='density')
+f, pxx = scisig.welch(signal_to_4th_power, fs=sample_rate/decimation, \
+                       return_onesided=False, nperseg=nperseg, scaling='density')
 f = (np.roll(f, len(f)/2))
 pxx = np.roll(pxx, len(pxx)/2)
 pxx = 10*np.log10(pxx)
@@ -288,24 +356,22 @@ plt.xlim([-2e3, 2e3])
 plt.title("Spectrum of signal to 4th power")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Magnitude (dB)")
+plt.tight_layout()
 
 
 # Plot complex samples from one channel
 plt.figure()
 ax = plt.subplot(111)
-start = int(1e6)
-stop = start + int(50e3)
-decim = 8*32
+
 matched_filtered_samples /= np.median(np.abs(matched_filtered_samples))
 phase_comp_samples /= np.median(np.abs(phase_comp_samples))
 
-plt.scatter(matched_filtered_samples.real[2000::8], matched_filtered_samples.imag[2000::8], marker='x', label='after MF')
-plt.scatter(phase_comp_samples.real[2000+sample_delay+2::8], phase_comp_samples.imag[2000+sample_delay+2::8], marker='x', label='timing recovery')
+plt.scatter(matched_filtered_samples.real[100*samples_per_symbol::samples_per_symbol], matched_filtered_samples.imag[100*samples_per_symbol::samples_per_symbol], marker='x', label='after MF')
+plt.scatter(phase_comp_samples.real[100*samples_per_symbol::samples_per_symbol], phase_comp_samples.imag[100*samples_per_symbol::samples_per_symbol], marker='x', label='timing recovery')
 plt.legend(loc='best')
 plt.title("Complex samples (10k samples)")
 plt.xlabel("Real")
 plt.ylabel("Imag")
 ax.set_aspect(aspect=1)
 plt.tight_layout()
-
 plt.show()

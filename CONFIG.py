89770976017bc48dcf24e79f8d7c065c36e4184d
Frank Bieberly <frank.bieberly@gmail.com>
2019-10-09 12:54:43 -0700

    update lat lon

--- CONFIG.py
+++ CONFIG.py
@@ -1,7 +1,7 @@
 
 
 # Ground station location
-lat = 47.245255
-lon = -122.469978
-alt = 0 
+lat = 0.0
+lon = 0.0
+alt = 0
 min_elevation = 5.0 # in degrees, in urban or heavily wooded areas, increase as appropriate

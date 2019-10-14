7d9423fdcc7f6a2e9cc2f462d8fab0ec7c35de16
Frank Bieberly <frank.bieberly@gmail.com>
2019-10-14 13:28:23 -0700

    adding realtime_plotting switch

--- CONFIG.py
+++ CONFIG.py
@@ -5,3 +5,6 @@ lat = 0.0
 lon = 0.0
 alt = 0
 min_elevation = 5.0 # in degrees, in urban or heavily wooded areas, increase as appropriate
+
+# Change this to false if you don't want the realtime receiver to plot anything
+realtime_plotting = True

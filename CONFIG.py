a833fc1516a35376ca2d5879a928c647d1b8a354
Frank Bieberly <frank.bieberly@gmail.com>
2020-05-20 16:19:18 -0700

    adding ip address info to config

--- CONFIG.py
+++ CONFIG.py
@@ -8,3 +8,7 @@ min_elevation = 5.0 # in degrees, in urban or heavily wooded areas, increase as 
 
 # Change this to false if you don't want the realtime receiver to plot anything
 realtime_plotting = True
+
+# IP address of processing computer for realtime_receiver_network.py
+ip_address = '192.168.1.100'
+port_number = 5556
